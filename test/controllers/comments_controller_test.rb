63ce7dada4c52221454be07ba6f9889f44bfef02
siHarv <harveycasane1@gmail.com>
2026-02-10 20:50:56 +0800

    fix: fix rails test warnings

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -1,13 +1,30 @@
 require "test_helper"
 
 class CommentsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get comments_create_url
-    assert_response :success
+  test "should create comment" do
+    user = users(:one)
+    post = posts(:one)
+
+    login_as(user, scope: :user)
+
+    assert_difference("Comment.count", 1) do
+      post post_comments_url(post), params: { comment: { body: "Test comment" } }
+    end
+
+    assert_redirected_to root_path
   end
 
-  test "should get destroy" do
-    get comments_destroy_url
-    assert_response :success
+  test "should destroy comment" do
+    user = users(:one)
+    post = posts(:one)
+    comment = comments(:one)
+
+    login_as(user, scope: :user)
+
+    assert_difference("Comment.count", -1) do
+      delete post_comment_url(post, comment)
+    end
+
+    assert_redirected_to root_path
   end
 end
