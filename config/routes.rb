3ca1fb47eda0d7caa4fa3961f6efa2ede4411e57
siHarv <harveycasane1@gmail.com>
2026-02-16 17:42:06 +0800

    feat: implemened profile for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get "profile/show"
   mount Motor::Admin => "/motor_admin"
   get "votes/create"
   get "votes/destroy"
   devise_for :users
+  resource :profile, only: [ :show ], controller: "profile"
   # RESTful routes for posts
   resources :posts do
     resources :comments, only: [ :create, :destroy, :edit, :update ]
