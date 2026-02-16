486ca4944804f2be6875ab4cc8492c33484e7cba
siHarv <harveycasane1@gmail.com>
2026-02-16 17:46:23 +0800

    fix: fixed rails test warning from dependabot

--- test/controllers/profile_controller_test.rb
+++ test/controllers/profile_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class ProfileControllerTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:one)
+    sign_in @user
+  end
+
   test "should get show" do
     get profile_show_url
     assert_response :success
