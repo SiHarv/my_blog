7cad955e022cacf2539ffbd16cc1e9f7f0493dcf
siHarv <harveycasane1@gmail.com>
2026-02-16 15:28:55 +0800

    remove: ommited remoev vote and user can remove vote by clicking the up/down vote again

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,13 +4,29 @@ class VotesController < ApplicationController
   def create
     @vote = @post.votes.find_or_initialize_by(user_id: current_user.id)
     vote_value = params[:value].to_i
-    if vote_value == 0
+
+    unless [ -1, 0, 1 ].include?(vote_value)
+      redirect_back fallback_location: root_path, alert: "Invalid vote value"
+      return
+    end
+
+    current_value = @vote.persisted? ? @vote.value : 0
+    new_value =
+      if vote_value == 0
+        0
+      elsif current_value == vote_value
+        0
+      else
+        current_value + vote_value
+      end
+
+    if new_value == 0
       @vote.destroy if @vote.persisted?
       message = "vote removed"
     else
-      @vote.value = vote_value
+      @vote.value = new_value
       if @vote.save
-        message = vote_value == 1 ? "Upvoted!" : "Downvoted!"
+        message = new_value == 1 ? "Upvoted!" : "Downvoted!"
       else
         message = "Error: #{@vote.errors.full_messages.join(", ")}"
       end
